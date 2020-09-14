0bb14193fc4030f17c96f4017750bddf1c95115c
Venkata Sai Prasanth Ippagunta <VenkataSaiPrasanth.Ippagunta@copart.com>
2020-09-13 23:20:54 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReverseGeocode from './ReverseGeocode'
 
-function App() {
+const handleResults = (results) => console.log(results)
+
+const onError = (errorType) => console.log(errorType)
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ReverseGeocode onFetchAddresses={handleResults} onError={onError}>
+      {({ getCurrentLocation, loading }) => (
+        <button onClick={getCurrentLocation} disabled={loading}>
+          Get Current Location
+        </button>
+      )}
+    </ReverseGeocode>
+  )
 }
-
-export default App;
---
+++ src/ReverseGeocode.js
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import { func } from 'prop-types'
+
+const geocodeLatLng = (lat, lng, onFetchAddresses, handleError) => {
+  const googleMaps = window.google && window.google.maps
+  if (googleMaps) {
+    const geocoder = new googleMaps.Geocoder()
+    geocoder.geocode(
+      {
+        location: {
+          lat,
+          lng,
+        },
+      },
+      (results, status) => {
+        if (status === 'OK') {
+          if (results[0]) {
+            onFetchAddresses(results)
+            handleError('')
+          } else {
+            handleError('noResultsFound')
+            onFetchAddresses([])
+          }
+        } else {
+          handleError('googleAddressError')
+        }
+      }
+    )
+  } else {
+    handleError('googleAddressError')
+  }
+}
+
+const getResults = (position, onFetchAddresses, handleError) => {
+  const { coords = {} } = position
+  const { latitude: lat, longitude: lng } = coords
+  if (lat && lng) {
+    geocodeLatLng(lat, lng, onFetchAddresses, handleError)
+  } else {
+    handleError('coordsUnavailable')
+  }
+}
+
+const getGeoLocationError = (error, handleError) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      return handleError('permissionDenied')
+    case error.POSITION_UNAVAILABLE:
+      return handleError('positionUnavailable')
+    case error.TIMEOUT:
+      return handleError('timeout')
+    default:
+      handleError('unknown')
+  }
+}
+
+const getLocation = (onFetchAddresses, handleError) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => getResults(position, onFetchAddresses, handleError),
+      (error) => getGeoLocationError(error, handleError)
+    )
+  } else {
+    handleError('geoLocationUnavailable')
+  }
+}
+
+const ReverseGeocode = (props) => {
+  const { onFetchAddresses, onError, children } = props
+  const [loading, setLoading] = useState(false)
+
+  const handleError = (type) => {
+    setLoading(false)
+    type && onError(type)
+  }
+
+  return children({
+    getCurrentLocation: () => {
+      setLoading(true)
+      getLocation(onFetchAddresses, handleError)
+    },
+    loading,
+  })
+}
+
+ReverseGeocode.propTypes = {
+  onFetchAddresses: func,
+  onError: func,
+  children: func,
+}
+
+export default ReverseGeocode
